2d46a6ad90f02bcb85438aa6e74e97160b8e6772
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-03 17:31:45 -0300

    Criado o arquivo saleService.test.js contendo os testes da camada Service de vendas

--- tests/unit/services/saleService.test.js
+++ tests/unit/services/saleService.test.js
@@ -4,5 +4,37 @@ const model = require('../../../models/saleModel');
 const service = require('../../../services/saleService');
 
 describe('Realizando teste da camada Service de vendas', () => {
-  describe('Testando a função addSale');
+  describe('Testando a função addSale', () => {
+    beforeEach(() => {
+      sinon.stub(model, 'addSale').returns({ saleId: 3 });
+      sinon.stub(model, 'soldProducts').returns({ productId: 2, quantity: 2 });
+    });
+    
+    afterEach(() => sinon.restore());
+
+    it('Verifica se um objeto é retornado', async () => {
+      const sale = await service.addSale([{ productId: 2, quantity: 2 }]);
+      
+      expect(sale).to.be.a('object');
+    });
+
+    it('Verifica se o código de resposta é 201', async () => {
+      const sale = await service.addSale([ { productId: 2, quantity: 2 }]);
+      
+      expect(sale.code).to.be.equal(201);
+    });
+
+    it('Verifica se o retorno na chave sold é um objeto', async () => {
+      const sale = await service.addSale([ { productId: 2, quantity: 2 }]);
+      
+      expect(sale.sold).to.be.a('object');
+    });
+
+    it('Verifica se é retornado corretamente o id e o produto vendido', async () => {
+      const sale = await service.addSale([ { productId: 2, quantity: 2 }]);
+      
+      expect(sale.sold).to.be.deep
+        .equal({ id: { saleId: 3 }, itemsSold: [{ productId: 2, quantity: 2 }]});
+    });
+  });
 });
