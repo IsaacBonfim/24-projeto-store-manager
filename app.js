46c2d6090fb96a881993d5f4441f84451dbb20e2
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-03 18:31:14 -0300

    Enviando o requisito 8

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const idValidation = require('./middlewares/saleProductValidate');
 const app = express();
 app.use(bodyParser.json());
 
-// não remova esse endpoint, é para o avaliador funcionar
+//! não remova esse endpoint, é para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.send();
 });
