9f71e863837120495f12c6925442db2c17d95ece
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-04 13:45:42 -0300

    Adicionado ao arquivo saleMock.js as informações dos demais estados das consultas para proseguir com os testes da camada Service e Controller

--- tests/unit/mocks/saleMock.js
+++ tests/unit/mocks/saleMock.js
@@ -1,4 +1,4 @@
-const salesMock = [
+const salesMockSC = [
   {
     "date": "2022-06-30T00:36:01.000Z",
     "sale_id": 1,
@@ -19,4 +19,53 @@ const salesMock = [
   }
 ];
 
-module.exports = salesMock;
+const salesMockCC = [
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "saleId": 1,
+    "productId": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "saleId": 1,
+    "productId": 2,
+    "quantity": 10
+  },
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "saleId": 2,
+    "productId": 3,
+    "quantity": 15
+  }
+];
+
+const saleIdMockSC = [
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "sale_id": 1,
+    "product_id": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "sale_id": 1,
+    "product_id": 2,
+    "quantity": 10
+  },
+];
+
+const saleIdMockCC = [
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "productId": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "productId": 2,
+    "quantity": 10
+  },
+]
+
+module.exports = { salesMockSC, salesMockCC, saleIdMockSC, saleIdMockCC };
