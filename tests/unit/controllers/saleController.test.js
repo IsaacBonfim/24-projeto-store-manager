333bb54bab1f87b49348953b14b218115f5d43d9
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-04 14:28:20 -0300

    Adicionado no arquivo saleController.test.js os testes referentes a função getAll

--- tests/unit/controllers/saleController.test.js
+++ tests/unit/controllers/saleController.test.js
@@ -2,6 +2,7 @@ const sinon = require('sinon');
 const { expect } = require('chai');
 const service = require('../../../services/saleService');
 const controller = require('../../../controllers/saleController');
+const { salesMockCC } = require('../mocks/saleMock');
 
 describe('Realizando teste da camada Controller de vendas', () => {
   describe('Testando a função addSale', () => {
@@ -24,6 +25,32 @@ describe('Realizando teste da camada Controller de vendas', () => {
       await controller.addSale(req, res);
       
       expect(res.status.calledWith(201)).to.be.true;
+    });
+  });
+
+  describe('Testando a função getAll', () => {
+    const res = {};
+    const req = {};
+
+    beforeEach(() => {
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+      
+      sinon.stub(service, 'getAll').resolves({ code: 200, sales: salesMockCC });
+    });
+
+    afterEach(() => sinon.restore());
+
+    it('Verifica se é retornado o código de resposta 200', async () => {
+      await controller.getAll(req, res);
+      
+      expect(res.status.calledWith(200)).to.be.true;
+    });
+
+    it('Verificar se é retornado o array de vendas', async () => {
+      await controller.getAll(req, res);
+      
+      expect(res.json.calledWith(salesMockCC)).to.be.true;
     });
   });
 });
