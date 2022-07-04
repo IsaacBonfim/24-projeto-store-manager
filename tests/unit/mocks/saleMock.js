6b25d750d4b6a4711813dc814fba6122403e860f
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-04 12:11:36 -0300

    Criado o arquivo saleMock.js para mockar as informações do banco de dados

---
+++ tests/unit/mocks/saleMock.js
@@ -0,0 +1,20 @@
+const salesMock = [
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "sale_id": 1,
+    "product_id": 1,
+    "quantity": 5
+  },
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "sale_id": 1,
+    "product_id": 2,
+    "quantity": 10
+  },
+  {
+    "date": "2022-06-30T00:36:01.000Z",
+    "sale_id": 2,
+    "product_id": 3,
+    "quantity": 15
+  }
+]
