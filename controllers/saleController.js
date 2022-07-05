a5f9c20a7cdc77ed7c39e63214bd3ca30e3bf12c
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-05 14:26:15 -0300

    Adicionado ao arquivo saleController.js a função updateSale, que realiza a requisição para atualização de uma venda

--- controllers/saleController.js
+++ controllers/saleController.js
@@ -22,6 +22,17 @@ const saleController = {
     
     res.status(code).json(sale);
   },
+  updateSale: async (req, res) => {
+    const { id } = req.params;
+    const { body } = req;
+    const { code, update, message } = await service.updateSale(id, body);
+
+    if (message) {
+      return res.statu(code).json({ message });
+    }
+
+    res.status(code).json(update);
+  },
   deleteSale: async (req, res) => {
     const { id } = req.params;
     const { code, message } = await service.deleteSale(id);
