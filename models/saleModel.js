adee24073e99d0482d0fa7708a95b173eb5f95cf
Isaac Oliveira <isaac.net.oliveira@hotmail.com>
2022-07-05 13:45:55 -0300

    Adicionado ao arquivo saleModel.js a função updateSale, que executa a query para atualização de vendas na tabela sales_products

--- models/saleModel.js
+++ models/saleModel.js
@@ -36,6 +36,16 @@ const saleModel = {
 
     return sales;
   },
+  updateSale: async ({ id, productId, quantity }) => {
+    const query = `UPDATE StoreManager.sales_products
+      SET quantity = ?
+      WHERE sale_id = ?
+      AND product_id = ?;`;
+    
+    await connection.execute(query, [quantity, id, productId]);
+    
+    return { productId, quantity };
+  },
   deleteSale: async (id) => {
     const query = 'DELETE FROM StoreManager.sales WHERE id = ?;';
 
